a5f6e171a9c83f7dbb270c3629f1523b322beccc
corytveten <corytveten@gmail.com>
2020-07-01 19:24:21 -0700

    set filter variable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 
 const buttons = document.querySelectorAll(".btn");
+const storeItems = document.querySelectorAll("#store-items");
 
 
 buttons.forEach(function(button) {
     button.addEventListener('click', function(e) {
     e.preventDefault();
+    const filter = e.target.dataset.filter
+    console.log(filter);
     })
-})
+});
+
