93f9cb20bae23fdd20bb799687b280183db6c6a6
corytveten <corytveten@gmail.com>
2020-07-03 17:27:35 -0700

    solved click eventlistener

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 
 const buttons = document.querySelectorAll(".btn");
-const storeItems = document.querySelectorAll("#store-items");
+const storeItems = document.querySelectorAll('.store-item');
 
 
 buttons.forEach(function(button) {
     button.addEventListener('click', function(e) {
     e.preventDefault();
-    const filter = e.target.dataset.filter
-    console.log(filter);
-    })
-});
+
+    const filtered = e.target.dataset.filter;
+    
+    storeItems.forEach((item) => {
+        if (filtered === 'all') {
+            item.style.display = 'block'
+            } else {
+                if (item.classList.contains(filtered)) {
+                item.style.display = 'block';
+                } else {
+                item.style.display = 'none';
+                }
+            }
+        })
+    });
+
+})
 
